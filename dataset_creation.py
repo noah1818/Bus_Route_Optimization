2adee3747973555ee718271ed93f648873afca28
noah1818 <86883969+noah1818@users.noreply.github.com>
2025-10-14 15:15:47 +0200

    Update dataset_creation.py

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,7 +1,6 @@
 # -----------------------------------------------------------------------------
 # Project:     Bus Route Optimization
 # File:        dataset_creation.py
-# Company:     Stratton Oakmont
 # Date:        September 13, 2024
 # Version:     2.2.3
 # Description: This script is used by the main.py file to create the distinct time groups.
